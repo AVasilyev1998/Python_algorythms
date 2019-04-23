0b0a50b9804f901ad8c6baab509be1c31f5c4041
AVasilyev1998 <avasilyev1998@mail.ru>
2019-04-23 12:47:28 +0300

    testing hamming on files

--- kurs_channel/test_files_coding.py
+++ kurs_channel/test_files_coding.py
@@ -5,13 +5,28 @@
 #     jpeg_file.write(res)
 
 
-from kurs_channel.Hamming_module import hamming_code,hamming_decode,binary_array_to_int,int_to_binary_array,push_nulls
+from kurs_channel.Hamming_module import hamming_code, hamming_decode, binary_array_to_int, push_nulls
+from kurs_channel.other_methods import old_int_to_binary_array, int_to_binary_array
 
 
 f = open('test.txt','rb')
 res = f.read()
-kfc_bytes = []
+res_bytes = []
 for i in range(len(res)):
-    # print(hamming_code(4, 7, res[i]))
-    # print(push_nulls(7, 11, int_to_binary_array(res[i])))
-    print(int_to_binary_array(res[i]))
+    res_bytes.extend(int_to_binary_array(8, res[i]))
+print(res_bytes)
+end_of_i = 0
+while len(res_bytes) > 11:
+    tmp_arr = res_bytes[0:10]
+    print(tmp_arr)
+    # TODO: do smth with tmp_arr
+    res_bytes = res_bytes[10:].copy()
+
+
+# def get_subarray(_from, _to, arr):
+#     return arr[_from:_to]
+#
+#
+# print(get_subarray(0, 10, res_bytes))
+print(res_bytes[0:10])
+
